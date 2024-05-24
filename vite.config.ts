79037e301a679181f9abce4469694c7bcafa7c31
uniboxx <unibox@duck.com>
2024-05-24 22:43:02 +0200

    refactored proj

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,12 @@
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   base: '/girasole-carousel/',
-  root: 'src',
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 8080,
-  },
-  build: {
-    minify: true,
-    assetsInlineLimit: 0,
-    outDir: '../dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
+
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
     },
   },
-  plugins: [],
 });
